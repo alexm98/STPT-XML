84d9943f81d79f72671bf14422e02e6cbaf6164f
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-01-25 17:35:49 +0200

    Updated stations and timetable with parent class.

--- src/main/java/core/StationsInteractor.java
+++ src/main/java/core/StationsInteractor.java
@@ -1,26 +1,17 @@
 package core;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import parsers.ParserUtils;
-import parsers.XPathUtils;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 
-public class StationsInteractor {
-    private Document document;
-    private XPathUtils xputils;
-    private ParserUtils putils;
+public class StationsInteractor extends Interactor {
 
     public StationsInteractor(String path_to_doc) throws
             ParserConfigurationException,
             JAXBException {
-        this.putils = new ParserUtils(path_to_doc);
-        this.document = this.putils.parseJAXB();
-        this.xputils = new XPathUtils(this.document);
+        super(path_to_doc);
     }
 
     public NodeList getAllStations() throws XPathExpressionException {
@@ -29,18 +20,5 @@ public class StationsInteractor {
 
     public NodeList getStation(Integer station_id) throws XPathExpressionException {
         return xputils.QueryXPath(String.format("//TransportStation[@id=%s]", station_id));
-    }
-
-    public Document getDocument(){
-        return this.document;
-    }
-
-    public void SaveDocument(String location) throws TransformerException {
-        try{
-            this.putils.SaveDoc(this.document, location);
-        }
-        catch (TransformerException e){
-            System.out.println(e.getMessage());
-        }
     }
 }
