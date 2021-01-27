f509fc9110ad03f13b5d815ac3ba5092bb32e2a5
Zu Jah <eng.zuelfikarjahjah@gmail.com>
2021-01-28 01:09:30 +0200

    Clean up Post & Delete & Put functionality

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,11 @@ app.controller("xmlCtrl", function ($scope, $http) {
     var x2js = new X2JS();
     var response = '';
     var element = ''
-    // $scope.enableStationInput = false;
 
     $scope.clear = function () {
         $scope.stationsResult = null;
         $scope.vehiclesResult = null;
-        $scope.tableByIdResult = null;
+        $scope.tablesResult = null;
 
         $scope.stationByIdResult = null;
         $scope.vehicleByIdResult = null;
@@ -22,33 +21,6 @@ app.controller("xmlCtrl", function ($scope, $http) {
         $scope.enableStationInput = null;
         $scope.enableTableInput = null;
 
-        $scope.enableStationDeleteInput = null;
-        $scope.enableVehicleDeleteInput = null;
-        $scope.enableTableDeleteInput = null;
-
-
-        $scope.enableTableUpdateInput = null;
-        $scope.enableVehicleUpdateInput = null;
-        $scope.enableStationUpdateInput = null;
-
-        $scope.enableStationAddInput = null;
-        $scope.enableVehicleAddInput = null;
-        $scope.enableTableAddInput = null;
-        
-
-
-        $scope.putStationID = null;
-        $scope.putLineID = null;
-        $scope.putLineName = null;
-        $scope.putRowName = null;
-        $scope.putFriendlyName = null;
-        $scope.putshortName = null;
-        $scope.putLat = null;
-        $scope.putLong = null;
-        $scope.putInvalid = null;
-        $scope.putVerficationData = null;
-        $scope.putGmaps = null;
-        $scope.putInfoComments = null;
     }
 
     $scope.showSearchInput = function (type) {
@@ -63,42 +35,6 @@ app.controller("xmlCtrl", function ($scope, $http) {
 
     }
     
-    $scope.showDeleteInput = function (type) {
-        $scope.clear();
-        if (type == 'station') {
-            $scope.enableStationDeleteInput = true;
-        } else if (type == 'vehicle') {
-            $scope.enableVehicleDeleteInput = true;
-        } else if (type == 'table') {
-            $scope.enableTableDeleteInput = true;
-        }
-
-    }
-
-    $scope.showUpdateInput = function (type) {
-        $scope.clear();
-        if (type == 'station') {
-            $scope.enableStationUpdateInput = true;
-        } else if (type == 'vehicle') {
-            $scope.enableVehicleUpdateInput = true;
-        } else if (type == 'table') {
-            $scope.enableTableUpdateInput = true;
-        }
-
-    }
-
-
-    $scope.showAddInputs = function (type) {
-        $scope.clear();
-        if (type == 'station') {
-            $scope.enableStationAddInput = true;
-        } else if (type == 'vehicle') {
-            $scope.enableVehicleAddInput = true;
-        } else if (type == 'table') {
-            $scope.enableTableAddInput = true;
-        }
-
-    }
 
 
 
@@ -150,7 +86,7 @@ app.controller("xmlCtrl", function ($scope, $http) {
 
 
     $scope.getStationById = function () {
-        
+        $scope.clear();
         basedUrl = 'http://localhost:9091/api/station/' + $scope.inputStaion;
         get(basedUrl).then(
             function (success) {
@@ -167,6 +103,7 @@ app.controller("xmlCtrl", function ($scope, $http) {
 
 
     $scope.getVehicleByID = function () {
+        $scope.clear();
         basedUrl = 'http://localhost:9091/api/vehicle/' + $scope.inputVehicle;
         get(basedUrl).then(
             function (success) {
@@ -182,6 +119,7 @@ app.controller("xmlCtrl", function ($scope, $http) {
 
 
     $scope.getTableByID = function () { 
+        $scope.clear();
         basedUrl = 'http://localhost:9091/api/timetable/' + $scope.inputTable;
         get(basedUrl).then(
             function (success) {
@@ -194,144 +132,6 @@ app.controller("xmlCtrl", function ($scope, $http) {
         );
 
     }
-
-
-
-    $scope.deleteStaion = function () { 
-        basedUrl = 'http://localhost:9091/api/station/' + $scope.deleteStationID;
-        del(basedUrl).then(
-            function (success) {
-                response = success.data.replaceAll("-", "_");
-                element = '</transport_station>'
-                $scope.stationsResult = xmlToJson(response, element);
-                $scope.enableStationDeleteInput = false;
-            },
-            function (error) {
-                console.log("error: ", error);
-            }
-        );
-
-    }
-
-    $scope.deleteVehicle = function () { 
-        basedUrl = 'http://localhost:9091/api/vehicle/' + $scope.deleteVehicleID;
-        del(basedUrl).then(
-            function (success) {
-                response = success.data.replaceAll("-", "_");
-                element = '</vehicle>'
-                $scope.vehiclesResult = xmlToJson(response, element);
-                $scope.enableVehicleDeleteInput = false;
-            },
-            function (error) {
-                console.log("error: ", error);
-            }
-        );
-
-    }
-
-    $scope.deleteTable = function () { 
-        basedUrl = 'http://localhost:9091/api/timetable/' + $scope.deleteTableID;
-        del(basedUrl).then(
-            function (success) {
-                response = success.data.replaceAll("-", "_");
-                element = '</timetable>'
-                $scope.tablesResult = xmlToJson(response, element);
-                $scope.enableTableDeleteInput = false;
-            },
-            function (error) {
-                console.log("error: ", error);
-            }
-        );
-
-    }
-
-
-
-    $scope.addStation = function () { 
-        $scope.clear();
-        basedUrl = 'http://localhost:9091/api/station';
-        
-
-        var params = {}
-        params['transport-station'] = {}
-
-        params['transport-station']['_id'] = $scope.putStationID
-        params['transport-station']['line-id'] = $scope.putLineID
-        params['transport-station']['line-name'] = $scope.putLineName
-
-        params['transport-station']['raw-station-name'] = $scope.putRowName
-        params['transport-station']['friendly-station-name'] = $scope.putFriendlyName
-        params['transport-station']['short-station-name'] = $scope.putshortName
-        
-        params['transport-station']['lat'] = $scope.putLat
-        params['transport-station']['long'] = $scope.putLong
-        params['transport-station']['invalid'] = $scope.putInvalid
-
-        params['transport-station']['verified'] = $scope.putVerified
-        params['transport-station']['verification-date'] = $scope.putVerficationData
-        params['transport-station']['gmaps-link'] = $scope.putGmaps
-        params['transport-station']['info-comments'] = $scope.putInfoComments
-
-
-
-        var xmlBodyRequet = x2js.json2xml_str(params);
-
-        put(basedUrl, xmlBodyRequet).then(
-            function (success) {
-                $scope.getStations();
-                
-            },
-            function (error) {
-                console.log("error: ", error);
-            }
-        );
-
-    }
-
-
-
-
-    $scope.updateStation = function () { 
-        $scope.clear();
-        basedUrl = 'http://localhost:9091/api/station/' + $scope.putReplaceStation;
-        
-
-        var params = {}
-        params['transport-station'] = {}
-
-        params['transport-station']['_id'] = $scope.putStationID
-        params['transport-station']['line-id'] = $scope.putLineID
-        params['transport-station']['line-name'] = $scope.putLineName
-
-        params['transport-station']['raw-station-name'] = $scope.putRowName
-        params['transport-station']['friendly-station-name'] = $scope.putFriendlyName
-        params['transport-station']['short-station-name'] = $scope.putshortName
-        
-        params['transport-station']['lat'] = $scope.putLat
-        params['transport-station']['long'] = $scope.putLong
-        params['transport-station']['invalid'] = $scope.putInvalid
-
-        params['transport-station']['verified'] = $scope.putVerified
-        params['transport-station']['verification-date'] = $scope.putVerficationData
-        params['transport-station']['gmaps-link'] = $scope.putGmaps
-        params['transport-station']['info-comments'] = $scope.putInfoComments
-
-
-
-        var xmlBodyRequet = x2js.json2xml_str(params);
-
-        put(basedUrl, xmlBodyRequet).then(
-            function (success) {
-                $scope.getStations();
-                
-            },
-            function (error) {
-                console.log("error: ", error);
-            }
-        );
-
-    }
-
 
 
 
@@ -357,23 +157,10 @@ app.controller("xmlCtrl", function ($scope, $http) {
 
 
 
-
-
     function get(url) {
         return $http.get(url);
     }
-    function del(url) {
-        return $http.delete(url);
-    }
-
-    function post(url, params) {
-        return $http.post(url, params);
-        // return $http.post(url, JSON.stringify(params));
-    }
-    function put(url, params) {
-        return $http.put(url, params);
-    }
-
+  
 
 
 
