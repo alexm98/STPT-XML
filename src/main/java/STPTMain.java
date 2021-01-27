75f9437dc5673975c2056160c627e3785ee0f8fa
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-01-27 21:37:28 +0200

    Cleaned main.

--- src/main/java/STPTMain.java
+++ src/main/java/STPTMain.java
@@ -9,32 +9,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class STPTMain {
-    public static void IterateNodeList(NodeList query_res){
-        for(int i = 0; i < query_res.getLength(); i++){
-            System.out.println(query_res.item(i).getNodeName() + " " +
-                    query_res.item(i).getAttributes().item(0) + " '" +
-                    query_res.item(i).getTextContent() + "'");
-        }
-    }
-
     public static void main(String args[]) throws Exception {
-        StationsInteractor stationsInteractor = new StationsInteractor("data/statii-ratt.xml");
-        VehiclesInteractor vehiclesInteractor = new VehiclesInteractor("data/vehicles.xml");
-        TimeTablesInteractor timetablesInteractor = new TimeTablesInteractor("data/timetables.xml");
-
-//        NodeList allStations = stationsInteractor.getAllStations();
-//        NodeList allVehicles = vehiclesInteractor.getAllVehicles();
-//        NodeList allTimetables = timetablesInteractor.getAllTimeTables();
-//
-//        stationsInteractor.prettyPrintNodeList(allStations);
-//        vehiclesInteractor.prettyPrintNodeList(allVehicles);
-//        timetablesInteractor.prettyPrintNodeList(allTimetables);
-
-//        Node foundStation = stationsInteractor.getStation(2810);
-//        stationsInteractor.prettyPrintNode(foundStation);
-
-//        Node foundVehicle = vehiclesInteractor.getVehicle(158);
-//        vehiclesInteractor.prettyPrintNode(foundVehicle);
         WebService ws = new WebService(
                 "data/vehicles.xml",
                 "data/timetables.xml",
